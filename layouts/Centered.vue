440920fce909e16cca50782b04774216af355251
brusnitsyn <andrew.brusnitsyn@gmail.com>
2024-12-09 16:28:35 +0900

    Rotate

--- layouts/Centered.vue
+++ layouts/Centered.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts"></script>
 
 <template>
-  <div class="max-h-[1280px] my-auto py-2 -rotate-90 h-screen absolute left-8 inset-y-0">
+  <div class="max-h-[1280px] my-auto py-2 -rotate-90 h-full absolute left-8 inset-y-0">
     <slot />
   </div>
 </template>
